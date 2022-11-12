2ceb37f3c9026feb41343a6d306416d02b0b4b5c
Chris Husslack <chris@husslack.de>
2022-11-12 16:53:27 +0100

    Added OpenTelemetry project.

---
+++ OpenTelemetry/OpenTelemetryConsole/Program.cs
@@ -0,0 +1,34 @@
+﻿using System.Diagnostics;
+
+using OpenTelemetry;
+using OpenTelemetry.Trace;
+using OpenTelemetry.Resources;
+using OpenTelemetry.Exporter;
+using Tools;
+
+// Define some important constants to initialize tracing with
+var serviceName = "MyCompany.MyProduct.MyService";
+var serviceVersion = "1.0.0";
+
+// Configure important OpenTelemetry settings and the console exporter
+using var tracerProvider = Sdk.CreateTracerProviderBuilder()
+    .AddSource(serviceName)
+    .SetResourceBuilder(
+        ResourceBuilder.CreateDefault()
+            .AddService(serviceName: serviceName, serviceVersion: serviceVersion))
+    .AddConsoleExporter()
+    .AddOtlpExporter(opt =>
+    {
+        opt.Protocol = OtlpExportProtocol.Grpc;
+    })
+    .Build();
+
+var MyActivitySource = new ActivitySource(serviceName);
+
+using var activity = MyActivitySource.StartActivity("SayHello");
+activity?.SetTag("foo", 1);
+activity?.SetTag("bar", "Hello, World!");
+activity?.SetTag("baz", new int[] { 1, 2, 3 });
+
+var w = new Worker();
+w.Work();
