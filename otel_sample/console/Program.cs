d884c06149726357019f8ca1fbd204f6f44b3fa1
Chris Husslack <chris@husslack.de>
2023-12-10 17:55:55 +0100

    Added otel project.

---
+++ otel_sample/console/Program.cs
@@ -0,0 +1,64 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Diagnostics;
+using library;
+using OpenTelemetry;
+using OpenTelemetry.Exporter;
+using OpenTelemetry.Resources;
+using OpenTelemetry.Trace;
+
+internal class Program
+{
+    private static readonly ActivitySource MyActivitySource = new ActivitySource("OTELSample.Console");
+    
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+
+        using var tracerProvider = Sdk.CreateTracerProviderBuilder()
+            .SetResourceBuilder(ResourceBuilder.CreateDefault().AddService(
+                serviceName: "OTELSampleConsole",
+                serviceVersion: "1.0.0"))
+            .AddSource("OTELSample.Console", "OTELSample.Library")
+            .AddHttpClientInstrumentation()
+            .AddConsoleExporter()
+            .AddOtlpExporter()
+            .Build();
+
+        using (var activity = MyActivitySource.StartActivity("Say Hello"))
+        {
+            activity?.SetTag("foo", 1);
+            activity?.SetTag("bar", "Hello, World!");
+            activity?.SetTag("baz", new int[] { 1, 2, 3 });
+            activity?.SetStatus(ActivityStatusCode.Ok);
+
+            Thread.Sleep(100);
+
+            Worker w = new Worker();
+            w.Work();
+
+            Thread.Sleep(200);
+
+            InternalWorker();
+
+            using (var client = new HttpClient())
+            {
+                client.GetStringAsync("http://localhost:5147/weatherforecast").Wait();
+            }
+        }
+    }
+
+    static void InternalWorker()
+        {
+            using (var activity = MyActivitySource.StartActivity("InternalWorker"))
+            {
+                activity?.SetTag("foo", 1);
+                activity?.SetTag("bar", "Hello, World!");
+                activity?.SetTag("baz", new int[] {1, 2, 3});
+                activity?.SetStatus(ActivityStatusCode.Ok);
+
+                Thread.Sleep(100);
+            }
+        }
+}
+
+
---
+++ otel_sample/library/Worker.cs
@@ -0,0 +1,23 @@
+﻿using System.Diagnostics;
+
+namespace library;
+
+    public class Worker
+    {
+        private static readonly ActivitySource MyActivitySource = new ActivitySource("OTELSample.Library");
+
+        public void Work()
+        {
+            using (var activity = MyActivitySource.StartActivity("Worker.Work"))
+            {
+                activity?.SetTag("foo", 1);
+                activity?.SetTag("bar", "Hello, from lib!");
+                activity?.SetTag("baz", new int[] { 1, 2, 3 });
+                activity?.SetStatus(ActivityStatusCode.Ok);
+
+                Thread.Sleep(100);
+
+                System.Console.WriteLine("Did some work.");
+            }
+        }
+    }
